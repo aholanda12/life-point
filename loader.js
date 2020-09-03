8df41efec2db33e02a2f843814a2d8197d39dd4d
Ashley Holanda <aholanda12@gmail.com>
2020-09-02 21:48:01 -0400

    fixed lint problems

--- loader.js
+++ loader.js
@@ -5,21 +5,21 @@ const fs = require("fs");
 const readFile = require("util").promisify(fs.readFile);
 
 // Syncing our database and logging a message to the user upon success
-db.sequelize.sync().then(() => {
-  
-  return readFile("./db/affirmSeed.sql", "utf8")
-})
-.then( fileData => {
-  // console.log(fileData);
-  fileData.split("\n").forEach( query => {
-    // db.sequelize.query(query, function(){
-    //   console.log("Ran: " + query);
-    // })
-    db.Affirmation.create({
-      quote: query.split("'")[1]
-    })
-    .then( data => {
-      console.log("Ran: " + query);
-    })
+db.sequelize.sync()
+  .then(() => {
+    return readFile("./db/affirmSeed.sql", "utf8");
+  })
+  .then(fileData => {
+    // console.log(fileData);
+    fileData.split("\n").forEach(query => {
+      // db.sequelize.query(query, function(){
+      //   console.log("Ran: " + query);
+      // })
+      db.Affirmation.create({
+        quote: query.split("'")[1]
+      }).then(data => {
+        console.log("Ran: " + query);
+        return data;
+      });
+    });
   });
-});
