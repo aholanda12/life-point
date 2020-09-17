59d2d02018b228e7606e71fad9950acacea21069
Ashley Holanda <aholanda12@gmail.com>
2020-09-16 21:53:34 -0400

    calendar functionality working for on click

---
+++ db/journalSeed.sql
@@ -0,0 +1,15 @@
+INSERT INTO journals (entry, date, createdAt, updatedAt, UserId) VALUES ('Lorem ipsum dolor sit amet, consectetur adipiscing elit.', '2020-09-13 00:00:00', curDate(), curDate(), 1);
+INSERT INTO journals (entry, date, createdAt, updatedAt, UserId) VALUES ('Lorem ipsum dolor sit amet, consectetur adipiscing elit.', '2020-09-14 00:00:00', curDate(), curDate(), 1);
+INSERT INTO journals (entry, date, createdAt, updatedAt, UserId) VALUES ('Lorem ipsum dolor sit amet, consectetur adipiscing elit.', '2020-09-15 00:00:00', curDate(), curDate(), 1);
+
+INSERT INTO gratefuls (journalID, one, two, three, four, five, createdAt, updatedAt) VALUES (2, 'My spouse', 'Atlas', 'Good Health'," "," ",curDate(), curDate());
+INSERT INTO gratefuls (journalID, one, two, three, four, five, createdAt, updatedAt) VALUES (3, 'Understanding', 'Friends', 'Mind', 'Books', 'Weekend',curDate(), curDate());
+INSERT INTO gratefuls (journalID, one, two, three, four, five, createdAt, updatedAt) VALUES (4, 'Mistakes', 'Having a Voice', 'Laughter', 'Communication', 'Learning',curDate(), curDate());
+
+INSERT INTO remembers (journalID, one, two, three, four, five, createdAt, updatedAt) VALUES (2, 'Do stuff', 'Do things', 'Remember'," "," ",curDate(), curDate());
+INSERT INTO remembers (journalID, one, two, three, four, five, createdAt, updatedAt) VALUES (3, 'Groceries', 'Call Mom', 'Renew License', 'Read', 'Homework',curDate(), curDate());
+INSERT INTO remembers (journalID, one, two, three, four, five, createdAt, updatedAt) VALUES (4, 'Live', 'Laugh', 'Love', 'Set Boundaries', 'Breathe',curDate(), curDate());
+
+INSERT INTO moods (journalID, mood, medication, hoursSleep, minutesExercise, minutesNapping, servingsCaffiene, servingsAlcohol, hoursTV, showered, brushedTeeth, selfCare, minutesSocial, headache, nausea, exhaustion, insomnia, appetite, menstruation, createdAt, updatedAt) VALUES (2, 2, false, 6, 55, 54, 3, 3, 2, true, true, true, 24, true, true, true, true, 3, true, curDate(), curDate());
+INSERT INTO moods (journalID, mood, medication, hoursSleep, minutesExercise, minutesNapping, servingsCaffiene, servingsAlcohol, hoursTV, showered, brushedTeeth, selfCare, minutesSocial, headache, nausea, exhaustion, insomnia, appetite, menstruation, createdAt, updatedAt) VALUES (3, 2, false, 6, 55, 54, 3, 3, 2, true, true, true, 24, true, true, true, true, 3, true, curDate(), curDate());
+INSERT INTO moods (journalID, mood, medication, hoursSleep, minutesExercise, minutesNapping, servingsCaffiene, servingsAlcohol, hoursTV, showered, brushedTeeth, selfCare, minutesSocial, headache, nausea, exhaustion, insomnia, appetite, menstruation, createdAt, updatedAt) VALUES (4, 2, false, 6, 55, 54, 3, 3, 2, true, true, true, 24, true, true, true, true, 3, true, curDate(), curDate());
--- public/js/calendar.js
+++ public/js/calendar.js
@@ -1,45 +1,61 @@
 const currentYear = new Date().getFullYear();
+const source = [];
 
-const calendar = new Calendar("#calendar", {
-  dataSource: [{
-    id: 0,
-    startDate: new Date(currentYear, 5, 10),
-    endDate: new Date(currentYear, 5, 10)
-  }]
+function getCalendarData(data) {
+  $.ajax({
+    url: "/api/calendar",
+    method: "GET",
+    data: { data: data }
+  })
+    .then(function (res) {
+      for (let i = 0;i < res.length;i++) {
+        const month = (res[i].Journal.date.slice(5, 7)) - 1;
+        const day = res[i].Journal.date.slice(8, 10);
+        source.push({
+          startDate: new Date(currentYear, month, day),
+          endDate: new Date(currentYear, month, day)
+        });
+      }
+      console.log(source);
+      const calendar = new Calendar("#calendar", {
+        dataSource: source
+      });
+      console.log(calendar);
+      return source;
+    });
+}
+
+function getDate(data) {
+  $.ajax({
+    url: "/api/calendar",
+    method: "GET",
+    data: { data: data }
+  })
+    .then(function (res) {
+      for (let i = 0;i < res.length;i++) {
+        const year = (res[i].Journal.date.slice(0, 4));
+        const month = (res[i].Journal.date.slice(5, 7));
+        const splitMonth = month.split("");
+        if (splitMonth[0] === "0") {
+          newMonth = splitMonth[1];
+        }
+        else {
+          newMonth = month;
+        }
+        const day = res[i].Journal.date.slice(8, 10);
+        console.log(newMonth + "/" + day + "/" + year);
+      }
+    });
+}
+
+getCalendarData();
+getDate();
+
+document.querySelector("#calendar").addEventListener("clickDay", function (e) {
+  console.log(e.date.toLocaleDateString());
 });
-console.log(calendar);
 
-document.querySelector("#calendar").addEventListener("clickDay", function() {
-  console.log(calendar._dataSource.id);
-});
-
-
-// const calendar = new Calendar("#calendar", {
-//   dataSource: getCalendarData()
-// });
-// console.log(calendar);
-
-// function getCalendarData(data) {
-//   $.ajax({
-//     url: "/api/calendar",
-//     method: "GET",
-//     data: { data: data }
-//   })
-//     .then(function (res) {
-//       console.log(res);
-//       const source = [];
-//       for (let i = 0;i < res.length;i++) {
-//         const month = (res[i].Journal.date.slice(5, 7)) - 1;
-//         const day = res[i].Journal.date.slice(8, 10);
-//         source.push({
-//           startDate: new Date(currentYear, month, day),
-//           endDate: new Date(currentYear, month, day)
-//         });
-//       }
-//       console.log(source);
-//       return source;
-//     });
-// }
+console.log(source);
 
 
 
